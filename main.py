9fd0a4647ee1b07bdc825ddfef7740ba9b1c00ed
zCheat <82244990+zCheat@users.noreply.github.com>
2021-04-09 21:24:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ web = input ("pagina: ")
 peticion = requests.get(web)
 html = peticion.text
 #crear archivo
-pagina = open('index.html', w)
+pagina = open('index.html', 'w')
 pagina.write(html)
 pagina.close()
 print ("pagina descargada")
